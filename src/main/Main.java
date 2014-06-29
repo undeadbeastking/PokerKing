6d0420a624f38d0e356f8ce54be4964fa5e9632b
Agatha <undeadbeastking@gmail.com>
2014-06-29 22:19:58 +0700

    Format code

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,13 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
         LoginInter logInter = new LoginInter();
-        //Customize Login Frame
-        logInter.setSize(600, 400);
-        logInter.setTitle("Poker King - The Game");
-        logInter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        logInter.setResizable(false);
-        logInter.setLocationRelativeTo(null);//Set center
-        logInter.setVisible(true);
 
     }
 }
