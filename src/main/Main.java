03b91fdc26cbc248246575dcd4c2b5542f87a2d9
Agatha <undeadbeastking@gmail.com>
2014-06-30 17:37:59 +0700

    Agatha tries to create a conflict

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,8 +6,8 @@ import javax.swing.*;
 
 public class Main {
 
-    // Master starts working
-    // The boss also demands attentions
+    // Agatha starts working
+    // Agatha also demands attentions
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
 
