8011482d92aafbfac9987005dd563d2647242c5b
Agatha <undeadbeastking@gmail.com>
2014-06-30 16:55:07 +0700

    Master commits 1

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,7 +5,10 @@ import view.MainFrame;
 import javax.swing.*;
 
 public class Main {
-    //Master
+
+
+    // Master codes something here
+    // Demand attentions
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
 
