836c6b80082f48ae24f2a3f73863d7c7ee8097d3
Agatha <undeadbeastking@gmail.com>
2014-06-30 17:27:24 +0700

    Try to create a conflict

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,8 @@ import javax.swing.*;
 
 public class Main {
 
+    // Master starts working
+    // The boss also demands attentions
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
 
