21dc605ac0c91a425af10d8b3c3058433ba0f2fd
Agatha <undeadbeastking@gmail.com>
2014-07-05 17:52:18 +0700

    Format code a bit

--- src/controller/LoginCon.java
+++ src/controller/LoginCon.java
@@ -13,28 +13,28 @@ import java.awt.event.*;
  */
 public class LoginCon {
 
-    private MainFrame mainFrame;
+    private MainFrame f;
 
-    public LoginCon(MainFrame f) {
-        this.mainFrame = f;
+    public LoginCon(MainFrame frame) {
+        this.f = frame;
 
         //Add FocusListener for Input Fields
-        mainFrame.getLoginPanel().getUsernameField().addFocusListener(new JFieldListener());
-        mainFrame.getLoginPanel().getPasswordField().addFocusListener(new JFieldListener());
+        f.getLoginPanel().getUsernameField().addFocusListener(new JFieldListener());
+        f.getLoginPanel().getPasswordField().addFocusListener(new JFieldListener());
 
         //Add MouseListener to Signup button
-        mainFrame.getLoginPanel().getSignUp().addMouseListener(new SignUpListener());
+        f.getLoginPanel().getSignUp().addMouseListener(new SignUpListener());
 
         //Add ActionListener to Signin button
-        mainFrame.getLoginPanel().getSignIn().addActionListener(new SigninListener());
+        f.getLoginPanel().getSignIn().addActionListener(new SigninListener());
     }
 
     private class SigninListener implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
             //Extract inputs
-            String username = mainFrame.getLoginPanel().getUsernameField().getText();
-            String password = String.valueOf(mainFrame.getLoginPanel().getPasswordField().getPassword());
+            String username = f.getLoginPanel().getUsernameField().getText();
+            String password = String.valueOf(f.getLoginPanel().getPasswordField().getPassword());
             boolean byPass = false;
 
             //Validate username and password
@@ -51,12 +51,12 @@ public class LoginCon {
             //Log in successfully?
             if(byPass){
                 //Remove text of Login panel's fields
-                mainFrame.getLoginPanel().loginPanelRefresh();
+                f.getLoginPanel().loginPanelRefresh();
                 //Replace Login Panel with SignUp Panel
-                mainFrame.remove(mainFrame.getLoginPanel());
+                f.remove(f.getLoginPanel());
 
-                if(mainFrame.getGamePanel() == null){
-                    mainFrame.initialize_GamePanel(username);
+                if(f.getGamePanel() == null){
+                    f.initialize_GamePanel(username);
 
                     /*
                     Switch the panel of the current player only
@@ -64,20 +64,20 @@ public class LoginCon {
                      */
                 } else {
                     //get current player panel
-                    PlayerPanel[] current = mainFrame.getGamePanel().getPlayersP();
+                    PlayerPanel[] current = f.getGamePanel().getPlayersP();
                     current[6].changeCurrentPlayer(username);
                 }
 
-                mainFrame.add(mainFrame.getGamePanel());
+                f.add(f.getGamePanel());
                 //Set suitable size for the frame and relocate it to center
-                mainFrame.setSize(Utils.GamePanel_width, Utils.GamePanel_height);
-                mainFrame.setLocationRelativeTo(null);
+                f.setSize(Utils.GamePanel_width, Utils.GamePanel_height);
+                f.setLocationRelativeTo(null);
                 //Notify MainFrame
-                mainFrame.validate();
-                mainFrame.repaint();
+                f.validate();
+                f.repaint();
 
             } else {
-                mainFrame.getLoginPanel().getLoginApproval().setText("*Wrong username or password");
+                f.getLoginPanel().getLoginApproval().setText("*Wrong username or password");
             }
         }
     }
@@ -89,8 +89,8 @@ public class LoginCon {
             JPasswordField passwordRef;
 
             //usernameField
-            if (mainFrame.getLoginPanel().getUsernameField() == e.getSource()) {
-                usernameRef = mainFrame.getLoginPanel().getUsernameField();
+            if (f.getLoginPanel().getUsernameField() == e.getSource()) {
+                usernameRef = f.getLoginPanel().getUsernameField();
                 if (usernameRef.getText().equals("Username...")) {
                     usernameRef.setText("");
                 }
@@ -99,8 +99,8 @@ public class LoginCon {
             }
 
             //passwordField
-            if (mainFrame.getLoginPanel().getPasswordField() == e.getSource()) {
-                passwordRef = mainFrame.getLoginPanel().getPasswordField();
+            if (f.getLoginPanel().getPasswordField() == e.getSource()) {
+                passwordRef = f.getLoginPanel().getPasswordField();
                 String pass = String.valueOf(passwordRef.getPassword());
                 if (pass.equals("Password...")) {
                     passwordRef.setText("");
@@ -116,8 +116,8 @@ public class LoginCon {
             JPasswordField passwordRef;
 
             //usernameField
-            if (mainFrame.getLoginPanel().getUsernameField() == e.getSource()) {
-                usernameRef = mainFrame.getLoginPanel().getUsernameField();
+            if (f.getLoginPanel().getUsernameField() == e.getSource()) {
+                usernameRef = f.getLoginPanel().getUsernameField();
                 if (usernameRef.getText().equals("")) {
                     usernameRef.setFont(Utils.hintFont);
                     usernameRef.setForeground(Utils.hintColor);
@@ -126,8 +126,8 @@ public class LoginCon {
             }
 
             //passwordField
-            if (mainFrame.getLoginPanel().getPasswordField() == e.getSource()) {
-                passwordRef = mainFrame.getLoginPanel().getPasswordField();
+            if (f.getLoginPanel().getPasswordField() == e.getSource()) {
+                passwordRef = f.getLoginPanel().getPasswordField();
                 if (passwordRef.getPassword().length == 0) {
                     passwordRef.setEchoChar((char) 0);
                     passwordRef.setFont(Utils.hintFont);
@@ -142,16 +142,16 @@ public class LoginCon {
         @Override
         public void mouseClicked(MouseEvent e) {
             //Remove text of Login panel's fields
-            mainFrame.getLoginPanel().loginPanelRefresh();
+            f.getLoginPanel().loginPanelRefresh();
             //Replace Login Panel with SignUp Panel
-            mainFrame.remove(mainFrame.getLoginPanel());
-            mainFrame.add(mainFrame.getSignUpPanel());
+            f.remove(f.getLoginPanel());
+            f.add(f.getSignUpPanel());
             //Set suitable size for the frame and relocate it to center
-            mainFrame.setSize(Utils.SignUpPanel_width, Utils.SignUpPanel_height);
-            mainFrame.setLocationRelativeTo(null);
+            f.setSize(Utils.SignUpPanel_width, Utils.SignUpPanel_height);
+            f.setLocationRelativeTo(null);
             //Notify MainFrame
-            mainFrame.validate();
-            mainFrame.repaint();
+            f.validate();
+            f.repaint();
         }
 
         @Override
@@ -164,12 +164,12 @@ public class LoginCon {
 
         @Override
         public void mouseEntered(MouseEvent e) {
-            mainFrame.getLoginPanel().getSignUp().setFont(Utils.hyperTextButtonFontEnter);
+            f.getLoginPanel().getSignUp().setFont(Utils.hyperTextButtonFontEnter);
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
-            mainFrame.getLoginPanel().getSignUp().setFont(Utils.hyperTextButtonFontNormal);
+            f.getLoginPanel().getSignUp().setFont(Utils.hyperTextButtonFontNormal);
         }
     }
 }
