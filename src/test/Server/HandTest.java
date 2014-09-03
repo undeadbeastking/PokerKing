16c9e937054a0c89508f751e6fbf92cc20c86f93
LamThienDuy <s3408657@rmit.edu.vn>
2014-09-03 21:26:15 +0700

    Update JUnit

---
+++ src/test/Server/HandTest.java
@@ -0,0 +1,85 @@
+package test.Server; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* Hand Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Sep 3, 2014</pre> 
+* @version 1.0 
+*/ 
+public class HandTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getId() 
+* 
+*/ 
+@Test
+public void testGetId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: display() 
+* 
+*/ 
+@Test
+public void testDisplay() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getCards() 
+* 
+*/ 
+@Test
+public void testGetCards() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getValues() 
+* 
+*/ 
+@Test
+public void testGetValues() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: handEvaluate() 
+* 
+*/ 
+@Test
+public void testHandEvaluate() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = Hand.getClass().getMethod("handEvaluate"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
---
+++ src/test/Server/ShowHandTest.java
@@ -0,0 +1,165 @@
+package test.Server; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* ShowHand Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Sep 3, 2014</pre> 
+* @version 1.0 
+*/ 
+public class ShowHandTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getFinalMoney() 
+* 
+*/ 
+@Test
+public void testGetFinalMoney() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getWinnerList() 
+* 
+*/ 
+@Test
+public void testGetWinnerList() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: findWinners() 
+* 
+*/ 
+@Test
+public void testFindWinners() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ShowHand.getClass().getMethod("findWinners"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: splitPot() 
+* 
+*/ 
+@Test
+public void testSplitPot() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ShowHand.getClass().getMethod("splitPot"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: handleTie(LinkedList<Hand> tieList, int strongestCom) 
+* 
+*/ 
+@Test
+public void testHandleTie() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ShowHand.getClass().getMethod("handleTie", LinkedList<Hand>.class, int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: findMaxInList(LinkedList<Hand> list, int position) 
+* 
+*/ 
+@Test
+public void testFindMaxInList() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ShowHand.getClass().getMethod("findMaxInList", LinkedList<Hand>.class, int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: addToResult(LinkedList<Hand> list, int max, int position) 
+* 
+*/ 
+@Test
+public void testAddToResult() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ShowHand.getClass().getMethod("addToResult", LinkedList<Hand>.class, int.class, int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: checkTie(LinkedList<Hand> tieList, int max, int position) 
+* 
+*/ 
+@Test
+public void testCheckTie() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ShowHand.getClass().getMethod("checkTie", LinkedList<Hand>.class, int.class, int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
